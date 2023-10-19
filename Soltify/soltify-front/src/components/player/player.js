fbf561213a5e47658a93e75c013bc57287332c79
PP2-21B030774 <azat.amenov@gmail.com>
2023-10-19 13:16:31 +0600

    Player first release

--- Soltify/soltify-front/src/components/player/player.js
+++ Soltify/soltify-front/src/components/player/player.js
@@ -7,6 +7,9 @@ const Player = () => {
   return (
       <div className={styles.background}>
           <div className={styles.background_layer}>
+              <div className={styles.close}>
+                  <ion-icon name="close-outline"></ion-icon>
+              </div>
               <div className={styles.song_img}>
                   <img src={songImg} alt={songImg} />
               </div>
@@ -21,8 +24,27 @@ const Player = () => {
                           <span className={styles.current}>1:24</span>
                           <span className={styles.duration}>2:32</span>
                       </div>
+                      <audio className={styles.main_audio}></audio>
                   </div>
-                  <div className={styles.player_navigation}></div>
+                  <div className={styles.player_navigation}>
+                      <ion-icon name="shuffle-outline" id={styles.random}></ion-icon>
+                      <div className={styles.center}>
+                          <ion-icon name="play-skip-back-outline"></ion-icon>
+                          <div className={styles.play_pause}>
+                              <ion-icon name="play" id={styles.play}></ion-icon>
+                              <ion-icon name="pause-outline" id={styles.pause}></ion-icon>
+                          </div>
+                          <ion-icon name="play-skip-forward-outline"></ion-icon>
+                      </div>
+                      <ion-icon name="repeat-outline" id={styles.repeat}></ion-icon>
+                  </div>
+                  <div className={styles.song_volume}>
+                      <ion-icon name="volume-off-outline"></ion-icon>
+                      <div className={styles.volume_progress}>
+                          <div className={styles.volume_progress_bar}></div>
+                      </div>
+                      <ion-icon name="volume-high-outline"></ion-icon>
+                  </div>
               </div>
           </div>
       </div>
