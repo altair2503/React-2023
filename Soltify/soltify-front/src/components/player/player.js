995e97a53416e4ff58451c84baa9bd0cf5c58a57
PP2-21B030774 <azat.amenov@gmail.com>
2023-10-20 20:01:29 +0600

    Player UI end

--- Soltify/soltify-front/src/components/player/player.js
+++ Soltify/soltify-front/src/components/player/player.js
@@ -1,54 +1,84 @@
-import React from "react";
-import styles from './player.module.css'
+import React, {useEffect, useState} from "react";
+import './player.css';
 
 import songImg from '../../assets/music.jpg'
 
 const Player = () => {
-  return (
-      <div className={styles.background}>
-          <div className={styles.background_layer}>
-              <div className={styles.close}>
-                  <ion-icon name="close-outline"></ion-icon>
-              </div>
-              <div className={styles.song_img}>
-                  <img src={songImg} alt={songImg} />
-              </div>
-              <div className={styles.player_controllers}>
-                  <div className={styles.song_details}>
-                      <div className={styles.name}>She's On My Mind (Acoustic)</div>
-                      <div className={styles.artist}>JP Cooper She's On My — Mind (Acoustics) - Single</div>
-                  </div>
-                  <div className={styles.song_progress}>
-                      <div className={styles.progress_bar}></div>
-                      <div className={styles.timer}>
-                          <span className={styles.current}>1:24</span>
-                          <span className={styles.duration}>2:32</span>
-                      </div>
-                      <audio className={styles.main_audio}></audio>
-                  </div>
-                  <div className={styles.player_navigation}>
-                      <ion-icon name="shuffle-outline" id={styles.random}></ion-icon>
-                      <div className={styles.center}>
-                          <ion-icon name="play-skip-back-outline"></ion-icon>
-                          <div className={styles.play_pause}>
-                              <ion-icon name="play" id={styles.play}></ion-icon>
-                              <ion-icon name="pause-outline" id={styles.pause}></ion-icon>
-                          </div>
-                          <ion-icon name="play-skip-forward-outline"></ion-icon>
-                      </div>
-                      <ion-icon name="repeat-outline" id={styles.repeat}></ion-icon>
-                  </div>
-                  <div className={styles.song_volume}>
-                      <ion-icon name="volume-off-outline"></ion-icon>
-                      <div className={styles.volume_progress}>
-                          <div className={styles.volume_progress_bar}></div>
-                      </div>
-                      <ion-icon name="volume-high-outline"></ion-icon>
-                  </div>
-              </div>
-          </div>
-      </div>
-  )
+
+    const [playerActive, setPlayerActive] = useState(false);
+
+    useEffect(() => {
+        if(localStorage.getItem("playerCondition") === "true") {
+            setPlayerActive(true)
+        } else setPlayerActive(false)
+    }, []);
+
+    function changeImageCursor(event) {
+        event.target.offsetWidth < 400 ? event.target.style.cursor = 'pointer' : event.target.style.cursor = 'default'
+    }
+
+    function playerActiveCondition(condition) {
+        if(condition) {
+            setPlayerActive(true)
+            localStorage.setItem("playerCondition", "true")
+        }
+        else {
+            setPlayerActive(false)
+            localStorage.setItem("playerCondition", "false")
+        }
+    }
+
+    return (
+        <div className={!playerActive ? "background mini" : "background"}>
+            <div className="background_layer">
+                <div className="close" onClick={() => playerActiveCondition(false)}>
+                    <ion-icon name="close-outline"></ion-icon>
+                </div>
+                <div className="song_img" onClick={() => playerActiveCondition(true)} onMouseMove={(event) => changeImageCursor(event)}>
+                    <img src={songImg} alt={songImg} />
+                </div>
+                <div className="player_controllers">
+                    <div className="song_details">
+                        <div className="name">She's On My Mind (Acoustic)</div>
+                        <div className="artist">JP Cooper — She's On My Mind</div>
+                    </div>
+                    <div className="song_center">
+                        <div className="song_progress">
+                            <div className="progress_bar"></div>
+                            <div className="timer">
+                                <span className="current">1:24</span>
+                                <span className="duration">2:32</span>
+                            </div>
+                            <audio className="main_audio"></audio>
+                        </div>
+                        <div className="player_navigation">
+                            <ion-icon name="shuffle-outline" id="random"></ion-icon>
+                            <div className="center">
+                                <ion-icon name="play-skip-back-outline"></ion-icon>
+                                <div className="play_pause">
+                                    <ion-icon name="play" id="play"></ion-icon>
+                                    <ion-icon name="pause-outline" id="pause"></ion-icon>
+                                </div>
+                                <ion-icon name="play-skip-forward-outline"></ion-icon>
+                            </div>
+                            <ion-icon name="repeat-outline" id="repeat"></ion-icon>
+                        </div>
+                    </div>
+                    <div className="player_options">
+                        <ion-icon name="ellipsis-horizontal"></ion-icon>
+                        <ion-icon name="scan-outline" onClick={() => playerActiveCondition(true)}></ion-icon>
+                    </div>
+                    <div className="song_volume">
+                        <ion-icon name="volume-off-outline"></ion-icon>
+                        <div className="volume_progress">
+                            <div className="volume_progress_bar"></div>
+                        </div>
+                        <ion-icon name="volume-high-outline"></ion-icon>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Player;
